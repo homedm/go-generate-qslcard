7dc29c539fffd8d033ffc183fa2358df26b60be5
homedm <weasel.wt@gmail.com>
2019-05-28 01:56:03 +0900

    Add: write call

--- main.go
+++ main.go
@@ -37,8 +37,18 @@ func main() {
 	pdf := gopdf.GoPdf{}
 	// 10.0 cm x 14.8 cm = 283.5 pt x 419.5 pt
 	pdf.Start(gopdf.Config{PageSize: gopdf.Rect{H: 283.5, W: 419.5}})
+	err = pdf.AddTTFFont("migu-1m", "../../../../../.fonts/migu-1m-regular.ttf")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = pdf.SetFont("migu-1m", "", 14)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	drawNewPage(&pdf)
+	writeHisCallsign(&pdf, "JJ1HGP")
 
 	pdf.WritePdf("qslcards.pdf")
 	//pdf.Line()
@@ -51,21 +61,32 @@ func drawNewPage(pdf *gopdf.GoPdf) {
 	pdf.SetLineWidth(2)
 
 	// draw his callsign box
-	x := 230.0
+	x := 240.0
 	y := 50.0
-	h := 25.0
-	w := 15.0
+	h1 := 25.0
+	w1 := 15.0
 	for i := 0.0; i < 6.0; i += 1.0 {
-		pdf.RectFromUpperLeftWithStyle(x+i*20, y, w, h, "single")
+		pdf.RectFromUpperLeftWithStyle(x+i*20, y, w1, h1, "single")
 	}
 
 	// draw QSO data form
 	x = 50.0
 	y = 100.0
-	w = 100
-	pdf.RectFromUpperLeftWithStyle(x, y, w, h, "single")
-	h2 := 40.0
-	for i := 0.0; i < 3; i += 1.0 {
-		pdf.RectFromUpperLeftWithStyle(x+i*w/3, y+h, w/3, h2, "single")
+	w1 = 300.0
+	h1 = 60.0
+	pdf.RectFromUpperLeftWithStyle(x, y, w1, h1, "single")
+	h2 := 20.0
+	pdf.SetLineWidth(1)
+	pdf.Line(x, y+h2, x+w1, y+h2)
+
+}
+
+func writeHisCallsign(pdf *gopdf.GoPdf, call string) {
+	x := 245.0
+	y := 70.0
+	for i, c := range call {
+		pdf.SetX(x + float64(i*20))
+		pdf.SetY(y)
+		pdf.Text(string(c))
 	}
 }
